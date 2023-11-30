186182b69dc85a9689085b9aa86e9f15fd0fde25
Sohyun Lim <shlim0410@gmail.com>
2023-11-30 11:35:31 +0000

    Title: 두 번째를 첫 번째로, Time: 228ms, Memory: 45MB, Status: Passed - CodeTree

---
+++ 231130/두 번째를 첫 번째로/second-to-first.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim();
+const first = input[0];
+const second = input[1];
+let res = "";
+for (let i = 0; i < input.length; i++) {
+    if (input[i] === second) res += first;
+    else res += input[i];
+}
+
+console.log(res);
