4ed265aed3582e1b4539d665819adcefefc361c3
Sohyun Lim <shlim0410@gmail.com>
2023-12-15 17:21:24 +0000

    Title: 대문자 소문자 바꾸기, Time: 214ms, Memory: 13MB, Status: Passed - CodeTree

---
+++ 231216/대문자 소문자 바꾸기/change-uppercase-and-lowercase.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim();
+let res = "";
+for (let i = 0; i < input.length; i++) {
+    if (input[i].charCodeAt(0) >= 'a'.charCodeAt(0) && input[i].charCodeAt(0) <= 'z'.charCodeAt(0)) {
+        res += input[i].toUpperCase();
+    }
+    else if (input[i].charCodeAt(0) >= 'A'.charCodeAt(0) && input[i].charCodeAt(0) <= 'Z'.charCodeAt(0)) {
+        res += input[i].toLowerCase();
+    }
+}
+
+console.log(res)
