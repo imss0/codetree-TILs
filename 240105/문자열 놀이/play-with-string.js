75ac5bf95f300058ba88c157783666f50862c8cf
Sohyun Lim <shlim0410@gmail.com>
2024-01-05 07:46:14 +0900

    Title: 문자열 놀이, Time: 212ms, Memory: 13MB, Status: Wrong Answer - CodeTree

---
+++ 240105/문자열 놀이/play-with-string.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split("\n");
+
+let [str, n] =[input[0].split(" ")[0], +input[0].split(" ")[1]]
+const queries = input.slice(1).map(item => item.split(" "));
+
+for (let i = 0; i < n; i++) {
+    const type = queries[i][0];
+    if (type === '1') {
+        const [a, b] = [+queries[i][1], +queries[i][2]];
+        str = str.slice(0, a - 1) + str[b - 1] + str.slice(a, b - 1) + str[a - 1] + str.slice(b)
+        console.log(str)
+    } 
+    else {
+        const [a, b] =[queries[i][1], queries[i][2]]
+        str = str.replaceAll(a, b);
+        console.log(str)
+    }
+}
+
+// qejmvwpxrsvqepzalxbdo
