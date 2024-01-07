0e6acb1b12fb0bdbca5bf2d209f64b70b4fde34d
Sohyun Lim <shlim0410@gmail.com>
2024-01-08 06:56:27 +0900

    Title: 격자 반대로 채우기, Time: 297ms, Memory: 45MB, Status: Passed - CodeTree

--- 240108/격자 반대로 채우기/grid-anti-fill.js
+++ 240108/격자 반대로 채우기/grid-anti-fill.js
@@ -3,7 +3,24 @@ const n = +fs.readFileSync(0).toString().trim();
 let res = Array(n).fill(0).map(() => Array(n).fill(0));
 let num = 1;
 
-for (let i = n - 1; i >= 0; i--) {
+if (n % 2 === 1) {
+    for (let i = n - 1; i >= 0; i--) {
+        if (i % 2 === 1) {
+            for (let j = 0; j < n; j++) {
+                res[j][i] = num;
+                num++;
+            }
+        } else {
+        for (let j = n - 1; j >= 0; j--) {
+                res[j][i] = num;
+                num++;
+            } 
+        }
+    }
+}
+
+if (n % 2 === 0) {
+    for (let i = n - 1; i >= 0; i--) {
     if (i % 2 === 1) {
         for (let j = n - 1; j >= 0; j--) {
             res[j][i] = num;
@@ -16,12 +33,8 @@ for (let i = n - 1; i >= 0; i--) {
         } 
     }
 }
+}
 
 for (let i = 0; i < n; i++) {
     console.log(res[i].join(" "));
 }
-
-// 4 4 - 4 3 - 4 2 - 4 1
-// 3 1  3 2 - 3 3 - 3 4 
-// 2 4 - 2 3 - 2 2 - 2 1
-// 1 1 - 1 2 - 1 3 - 1 4
