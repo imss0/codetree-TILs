78b5938cd4b292fbbef816a88ad29f2266698c1b
Sohyun Lim <shlim0410@gmail.com>
2024-02-21 18:22:35 +0000

    Title: 19단 출력, Time: 217ms, Memory: 47MB, Status: Passed - CodeTree

---
+++ 240222/19단 출력/nineteen-times-table.js
@@ -0,0 +1,9 @@
+for (let i = 1; i <= 19; i++) {
+    let str = "";
+    for (let j = 1; j <= 19; j++) {
+        str += `${i} * ${j} = ${i * j} `
+        if (j % 2 === 0) str += "\n"
+        else if (j % 2 === 1 && j !== 19) str += "/ "
+    }
+    console.log(str)
+}
