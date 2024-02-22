7bcad427b5912f36fdf3c9bb8bfe00e4104b90cc
Sohyun Lim <shlim0410@gmail.com>
2024-02-22 17:46:20 +0000

    Title: 약수의 개수가 3개인 수, Time: 244ms, Memory: 14MB, Status: Passed - CodeTree

--- 240223/약수의 개수가 3개인 수/square-of-prime-number.js
+++ 240223/약수의 개수가 3개인 수/square-of-prime-number.js
@@ -4,7 +4,7 @@ const [start, end] = fs.readFileSync(0).toString().split(" ").map(Number);
 let cnt = 0;
 for (let i = start; i <= end; i++) {
     let partCnt = 0;
-    for (let j = 1; j < i; j++) {
+    for (let j = 1; j <= i; j++) {
         if (i % j === 0) {
             partCnt++;
         }
