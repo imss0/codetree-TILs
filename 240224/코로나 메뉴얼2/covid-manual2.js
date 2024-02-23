3b4680816ff1cb9be6f87d84cee60470587ce444
Sohyun Lim <shlim0410@gmail.com>
2024-02-23 18:11:24 +0000

    Title: 코로나 메뉴얼2, Time: 171ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240224/코로나 메뉴얼2/covid-manual2.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().split("\n");
+
+let [A, B, C, D, E] = [0, 0, 0, 0, false]
+for (let i = 0; i < input.length; i++) {
+    let symptom = input[i].split(" ")[0];
+    let temperature = +input[i].split(" ")[1];
+    if (symptom === 'Y') {
+        if(temperature >= 37) {
+                A++;
+                if (A >= 2) E = true;
+        }
+        else C++;
+    } else {
+        if (temperature >= 37) {
+            B++;
+        } else D++;
+    }
+}
+console.log(A, B, C, D, E === true ? 'E' : "")
