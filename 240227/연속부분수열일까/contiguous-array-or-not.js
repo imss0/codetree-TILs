b4a75ec81570239827424b7535536259f1f2cdec
Sohyun Lim <shlim0410@gmail.com>
2024-02-26 22:03:55 +0000

    Title: 연속부분수열일까, Time: 172ms, Memory: 18MB, Status: Passed - CodeTree

--- 240227/연속부분수열일까/contiguous-array-or-not.js
+++ 240227/연속부분수열일까/contiguous-array-or-not.js
@@ -9,6 +9,19 @@ for (let i = 0; i < n1 - n2 + 1; i++) {
     const arr1 = input[1].split(" ").map(Number)
     const arr2 = input[2].split(" ").map(Number);
 
-    if (arr1[i] === arr2[i] && arr1[i+1] === arr2[i+1]) result = 'Yes'
+    let part = arr1.slice(i, i+n2);
+    let res = 0;
+    for (let j = 0; j < part.length; j++) {
+        if (part[j] === arr2[j]) {
+            // console.log('same')
+            res = res + 0;
+        }
+        else {
+            // console.log('diff')
+            res = res + 1;
+        }
+
+    }
+    if (res === 0) result = 'Yes';
 }
 console.log(result)
