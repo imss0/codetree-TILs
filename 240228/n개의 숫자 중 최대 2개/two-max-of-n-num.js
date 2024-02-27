6378654d6acc02f1f26045c0d3e6163cbcd7ccb0
Sohyun Lim <shlim0410@gmail.com>
2024-02-27 21:02:15 +0000

    Title: n개의 숫자 중 최대 2개, Time: 200ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240228/n개의 숫자 중 최대 2개/two-max-of-n-num.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const inputs = fs.readFileSync(0).toString().trim().split("\n");
+const n = +inputs[0]
+const list = inputs[1].split(" ").map(Number);
+
+let max = list[0]
+let second = -1000000000;
+for (let i = 1; i < n; i++) {
+    if (list[i] >= max) max = list[i];
+    else if (list[i] >= second) second = list[i];
+}
+
+console.log(max, second)
