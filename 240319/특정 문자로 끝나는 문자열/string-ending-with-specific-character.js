b09d287054591af300babad9056a400f733e7e41
Sohyun Lim <shlim0410@gmail.com>
2024-03-18 22:20:22 +0000

    Title: 특정 문자로 끝나는 문자열, Time: 166ms, Memory: 15MB, Status: Passed - CodeTree

--- 240319/특정 문자로 끝나는 문자열/string-ending-with-specific-character.js
+++ 240319/특정 문자로 끝나는 문자열/string-ending-with-specific-character.js
@@ -1,8 +1,11 @@
 const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim().split("\n");
 const keyChar = input[10];
+let isExist = false;
 for (let i = 0; i < 10; i++) {
     if (input[i][input[i].length - 1] === keyChar) {
+        isExist = true;
         console.log(input[i])
     }
 }
+if (!isExist) console.log('None')
